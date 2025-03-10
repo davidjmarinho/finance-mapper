c6e36aabc4c9e2827beebd53c2e4bb445c117625
David Pereira <davidjmarinhopereira@gmail.com>
2025-03-10 19:28:07 -0300

    Adicionando projeto mobile e api

---
+++ mobile/FinanceMapperApp/FinanceMapperApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FinanceMapperApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
